9ba9325c2edea5c564aab2225b440f1892a5bbf8
Luke Clewlow <luke.clewlow@gmail.com>
2015-06-01 11:39:40 +0100

    update config to allow CORS

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+require "rails/all"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -16,6 +17,13 @@ Bundler.require(*Rails.groups)
 
 module EmberAPI
   class Application < Rails::Application
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
