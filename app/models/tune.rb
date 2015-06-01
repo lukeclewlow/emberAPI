bd35a0eb4d960883a29cdad37ff449a63b81154f
Luke Clewlow <luke.clewlow@gmail.com>
2015-06-01 15:24:09 +0100

    setup first tests for model

--- app/models/tune.rb
+++ app/models/tune.rb
@@ -1,2 +1,4 @@
 class Tune < ActiveRecord::Base
+	validates :artist, :title, :link, :blog, presence: true
+	validates :link, uniqueness: true
 end
--- spec/models/tune_spec.rb
+++ spec/models/tune_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require "spec_helper"
 
-RSpec.describe Tune, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Tune do
+  it "should have a factory" do
+    expect(FactoryGirl.build(:tune)).to be_valid
+  end
+
+  context "validations" do
+    it { should validate_uniqueness_of(:link) }
+    it { should validate_presence_of(:artist) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:blog) }
+    it { should validate_presence_of(:link) }
+  end
 end
