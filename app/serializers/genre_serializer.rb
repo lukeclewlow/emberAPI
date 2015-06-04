6e3fa0c4dad8aad5e0353b133bfa46277809ddf7
Luke Clewlow <luke.clewlow@gmail.com>
2015-06-04 11:18:10 +0100

    updating serializers to try and play friendly with ember

--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,3 +1,5 @@
 class GenreSerializer < ActiveModel::Serializer
-  attributes :id, :name, :tunes
+	embed :ids, :include => true
+  attributes :id, :name
+  has_many :tunes
 end
--- app/serializers/tune_serializer.rb
+++ app/serializers/tune_serializer.rb
@@ -1,3 +1,4 @@
 class TuneSerializer < ActiveModel::Serializer
-  attributes :id, :artist, :title, :link, :blog, :genre
+  attributes :id, :artist, :title, :link, :blog, :genre_id
+  # has_one :genre
 end
